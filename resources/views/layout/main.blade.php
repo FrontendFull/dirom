c3585344231ba16316d7e35dc92778b149bfeb6b
yura <work.sergeevv@gmail.com>
2024-10-08 17:02:50 +0200

    benefits

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -28,6 +28,26 @@
             @endfor
         </div>
     </div>
+    <div class="benefits-division container my-content">
+        <div class="h1 title-section">Працювати з <span>Dirom</span> <div class="label">– це отримувати чесний та відповідальний сервіс</div></div>
+        <div class="benefits-mesh">
+            @for ($i = 1; $i <= 4; $i++)
+            <div class="benefit-card">
+                <div class="benefit-card__image">
+                    <img src="https://picsum.photos/355/450" alt="#">
+                </div>
+                <div class="benefit-card__info">
+                    <div class="benefit-card__title">
+                        {{ fake()->sentence(5) }}
+                    </div>
+                    <div class="benefit-card__text">
+                        {{ fake()->sentence(10) }}
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+    </div>
     <div class="seo-division container my-content">
         <div class="seo-text ckeditor">
             <h1>Більше про <span>компанію</span></h1>
